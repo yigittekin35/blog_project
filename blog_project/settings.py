dc28b22ccb43a9104ef2fe124fa63e06b69ec4df
Yigit Atahan Tekin <yigitatahantekin@gmail.com>
2022-07-17 21:24:23 +0300

    Medium style editor, google fonts, bootstrap eklenmesi, crud methodlarının çağırılması.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+STATIC_DIR = Path.joinpath(BASE_DIR, "static")
+TEMPLATE_DIR = Path.joinpath(BASE_DIR, "templates")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+   STATIC_DIR,
+]
+
+LOGIN_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
